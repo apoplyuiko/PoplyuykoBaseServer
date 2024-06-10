dadce954a4e64caf922991a322b35da2c899afe3
Andrey Poplyuyko <a.poplyuiko@dunice.net>
2024-06-10 14:40:48 +0300

    first commit

---
+++ src/main/java/com/example/Poplyuiko_base_server/PoplyuikoBaseServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Poplyuiko_base_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoplyuikoBaseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoplyuikoBaseServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Poplyuiko_base_server/PoplyuikoBaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Poplyuiko_base_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoplyuikoBaseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
